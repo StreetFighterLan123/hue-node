0b152561f2bbe70051c2f3a2b9fc7019b0da2b08
Austin <austin.simon@optonline.net>
2020-08-01 21:15:51 -0400

    Update and rename color.ts to toggle.ts

--- examples/color.ts
+++
@@ -1,8 +0,0 @@
-import { Hue } from '../lib'
-
-const hue = new Hue('username', 'ip')
-
-// number, hue, satuation, brightness
-hue.changeColor(8, 270, 200, 200).then(function (response) {
-    console.log('Color changed')
-});
